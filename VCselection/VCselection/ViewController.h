cc0cf04e390252d475bab0ccd8256e4559500f17
zhangjie1 <zhangjie1@guahao.com>
2017-12-18 17:19:25 +0800

    star

---
+++ VCselection/VCselection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VCselection
+//
+//  Created by 张杰 on 2017/12/18.
+//  Copyright © 2017年 张杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VCselection/VCselection/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  VCselection
+//
+//  Created by 张杰 on 2017/12/18.
+//  Copyright © 2017年 张杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor yellowColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
